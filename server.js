03327669d0d37687964a5aba262d8107b5630fcf
Aubri Henley <aubri.henley@gmail.com>
2021-10-12 17:58:36 -0400

    updated file route in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const Workout = require("./workout.js");
+const Workout = require("./models/workout");
 const app = express();
 
 app.use(logger("dev"));
