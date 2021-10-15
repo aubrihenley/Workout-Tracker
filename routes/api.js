4994fb1eb3d8e7273a331f2fe67045c78325935c
Aubri Henley <aubri.henley@gmail.com>
2021-10-15 17:55:40 -0400

    setting up what routes are needed in routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -2,20 +2,16 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 //route to get last workout
-router.get("/:id", (req, res) => {
-  Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
 
 //route to add to most recent workout
 
 //route to combine weight from last 7 workouts
 
+db.workouts.aggregate({
+  $addFields: {
+    totalWeight: { $sum: "$weight" },
+  },
+});
 //route to combine duration of last 7 workouts
 
 module.exports = router;
